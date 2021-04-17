1fa10b724f2ca80927e5e287da696ba0a7e1f134
flebuster <flebuster@tut.by>
2021-04-17 15:04:22 +0300

    3

--- js/sript.js
+++ js/sript.js
@@ -3,6 +3,7 @@ var ulSpisok = document.getElementById('list');
 var spans = document.getElementsByTagName('span');
 var saveBtn = document.getElementById('save');
 var clearBtn = document.getElementById('clear');
+var strongList = document.getElementsByTagName('strong');
 
  var day=new Array("Воскресенье","Понедельник","Вторник",
  "Среда","Четверг","Пятница","Суббота");
@@ -16,7 +17,7 @@ inputData.addEventListener("keypress", function (keyPressed) {
 if(keyPressed.which === 13) {
     var newLi = document.createElement('li');
     var newSpan = document.createElement('span'); 
-    var newDate = document.createElement('span');
+    var newDate = document.createElement('strong');
         
         newSpan.innerHTML = 'Delete    '; //как сделать пробел
         newDate.innerHTML = (day[date.getDay()]+" " +date.getDate()+ " " + month[date.getMonth()]
@@ -25,22 +26,41 @@ if(keyPressed.which === 13) {
     var newTodo = this.value;
      this.value = '';
 
-    ulSpisok.appendChild(newLi).append(newSpan, newTodo, newDate); 
+    if(newTodo.trim() === ''){ // удалили все пробелы в веденной строке, и если в итоге она окажется пустой тогда ничего не добавляем в список
+        alert('Введите задачу!');
+    } else{
+        ulSpisok.appendChild(newLi).append(newSpan, newTodo, newDate); 
+    }
+
+
+
+    
  
     deleteTodo();
+    editTodo();
     }
 
 });
 
 
-
- function deleteTodo(){
+function deleteTodo(){
     for (let span of spans){
         span.addEventListener('click', function(){
             span.parentElement.remove();
             event.preventDefault();
         })
     }
+
+ };
+
+ function editTodo(){
+        document.addEventListener('click', function(event){
+            if(event.target.tagName === 'STRONG'){
+                event.target.classList.toggle('show');
+                event.preventDefault();
+            }
+           
+        })
 
  };
 
@@ -59,8 +79,14 @@ clearBtn.addEventListener('click', function(){
 
 deleteTodo();
 
-loadTodo();
+editTodo();
 
+// loadTodo();
+
+
+
+
+ 
 
 
 
